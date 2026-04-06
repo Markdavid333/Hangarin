392d66be445f96721f8cb0c4c71a8c3fe22e6e69
markdavid <uzumakib094@gmail.com>
2026-04-06 11:01:26 +0800

    Added PWA icons

--- hangarin_project/urls.py
+++ hangarin_project/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('pwa.urls')),
     path('', include('tasks.urls')),
     path('accounts/', include('allauth.urls')),
 ]
